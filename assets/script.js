7e473aabfab5bee89e7f5f14e0f2bab7fd63f9f1
Igor Marques de Azevedo <igormarquesdeazevedo11@gmail.com>
2022-10-21 15:31:27 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+let total =0
+
+function incremento(){
+    total ++
+    document.getElementById('count-btn').innerHTML = total 
+        
+}
+
+function decremento(){
+    total --
+    document.getElementById('count-btn').innerHTML = total
+}
+
